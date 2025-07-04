d3ecd79ddbed52636094f0ed07c4b1aac5603c6a
Borges Richard <borges.exe1@gmail.com>
2025-07-04 12:54:10 -0300

    Adiciona arquivos de estilo e componentes ao projeto Svelte, substituindo HelloWorld por Desktop e implementando lógica de fundo dinâmica.

--- svelte-source/src/App.svelte
+++ svelte-source/src/App.svelte
@@ -1,18 +1,33 @@
 <script lang="ts">
+  import { isEnvBrowser } from "@utils/misc";
   import VisibilityProvider from '@providers/VisibilityProvider.svelte';
-  import HelloWorld from '@components/base/HelloWorld.svelte';
+  import Desktop from '@components/Desktop.svelte';
   import { debugData } from '@utils/debugData';
 
-  debugData([
-    {
-      action: 'setVisible',
-      data: true,
-    },
-  ]);
+  debugData([{ action: 'setVisible', data: true }]);
+
+  let background = "";
+  if (isEnvBrowser()) {
+    background = "url('https://localhost/image.png')";
+  } else {
+    background = "none";
+  }
+
+  document.documentElement.style.height = "100%";
+  document.body.style.height = "100%";
+  document.body.style.margin = "0";
+  document.body.style.padding = "0";
+  document.body.style.width = "100vw";
+  document.body.style.height = "100vh";
+  document.body.style.backgroundImage = background;
+  document.body.style.backgroundSize = "cover";
+  document.body.style.backgroundColor = "transparent";
+  document.body.style.backgroundPosition = "center";
+  document.body.style.backgroundRepeat = "no-repeat";
 </script>
 
 <main>
   <VisibilityProvider>
-    <HelloWorld />
+    <Desktop />
   </VisibilityProvider>
 </main>
---
+++ svelte-source/src/components/Desktop.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Name from '@components/base/Name.svelte';
+</script>
+
+<div class="container">
+  <Name/>
+</div>
+
+<style>
+  .container {
+    width: 100%;
+    height: 100%;
+  }
+</style>
